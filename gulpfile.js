c1f4b47f5fbe273d65b5e52ffbb63bb2857f7cac
ovhoffer <karilanko@wp.pl>
2023-07-28 17:23:15 +0200

    adding basic files

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+const gulp = require('gulp');
+const sass  = require('gulp-sass')(require('sass'));
+const sourcemaps = require('gulp-sourcemaps');
+const watch = require('gulp-watch');
+
+gulp.task('sass-compile', function () {
+   return gulp.src('./src/**/*.scss')
+      .pipe(sourcemaps.init())
+      .pipe(sass().on('error', sass.logError))
+   //   .pipe(sourcemaps.write('./'))
+      .pipe(gulp.dest('./dist/css'))
+      //.pipe(gulp.)
+})
+gulp.task('watch', function () {
+   gulp.watch('./src/**/*.scss', gulp.series('sass-compile'))
+})
